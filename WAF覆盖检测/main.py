88f0d449e29acce80604c832ca0d9a0e8fc9e5c4
cyan <cyan>
2021-06-27 16:47:48 +0800

    update

--- WAF覆盖检测/main.py
+++ WAF覆盖检测/main.py
@@ -14,12 +14,22 @@ requests.packages.urllib3.disable_warnings()
 
 FILE = "./urls.txt"
 VUL_PATH = "/root.exe"
-WAF_STRING = ["已被阻断", "店铺", "拦截ID", "html"]
+WAF_STRING = ["SimSun;\">无法显示此页", "拦截ID"]
 TIME_OUT = 8
-WORKERS = 50
+WORKERS = 10
+FOLLOW_REDIRECTS = True
 NAME_SERVER = ["223.5.5.5", "8.8.8.8"]
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) " \
              "Chrome/89.0.4389.90 Safari/537.36"
+
+def waf_func(resp, ret):
+    for key in WAF_STRING:
+        if key in resp.text:
+            ret["matched_string"] = key
+            ret["waf_detected"] = True
+            return ret
+    ret["waf_detected"] = False
+    return ret
 
 #########################################################################################################
 
@@ -33,6 +43,20 @@ def getipv6(domain):
         for i in AAAA_recorder.response.answer:
             for j in i.items:
                 if j.rdtype == 28:
+                    ret.append(j.address.replace("\n", " "))
+    except Exception:
+        pass
+    return ret
+
+def getipv4(domain):
+    ret = []
+    try:
+        my_resolver = dns.resolver.Resolver()
+        my_resolver.nameservers = NAME_SERVER
+        AAAA_recorder = my_resolver.resolve(domain, 'A')
+        for i in AAAA_recorder.response.answer:
+            for j in i.items:
+                if j.rdtype == 1:
                     ret.append(j.address.replace("\n", " "))
     except Exception:
         pass
@@ -55,13 +79,14 @@ def send_req(url, ipv6=True):
     url = url.rstrip("/") + VUL_PATH
     ret = {
         "origin_url": url,
-        "is_ipv6": None,
-        "dst_ip": None,
+        "is_ipv6": ipv6,
+        "sock_ip": None,
         "waf_detected": None,
         "matched_string": None,
         "status_code": None,
-        "final_url": None,
         "requests_error": None,
+        "ns_record": None,
+        "final_url": None,
     }
 
     try:
@@ -71,99 +96,90 @@ def send_req(url, ipv6=True):
             timeout=TIME_OUT,
             verify=False,
             stream=True,
-            allow_redirects=True,
+            allow_redirects=FOLLOW_REDIRECTS,
             headers={
                 "User-Agent": USER_AGENT
             }
         )
+        sock_ip = resp.raw._connection.sock.getpeername()[0]
+    except AttributeError:
+        sock_ip = None
+        ret["ns_record"] = ",".join(getipv6(tldextract.extract(url).fqdn)) if ipv6 else ",".join(getipv4(tldextract.extract(url).fqdn))
     except Exception as e:
+        ret["ns_record"] = ",".join(getipv6(tldextract.extract(url).fqdn)) if ipv6 else ",".join(getipv4(tldextract.extract(url).fqdn))
         ret["requests_error"] = e.__class__.__name__
         return ret
     ret["is_ipv6"] = ipv6
     ret["final_url"] = resp.url
     ret["status_code"] = resp.status_code
-    dst_ip = resp.raw._connection.sock.getpeername()[0]
-    if dst_ip.startswith("::ffff:"):
-        dst_ip = dst_ip.replace("::ffff:", "")
-    ret["dst_ip"] = dst_ip
-    # print(resp.text)
-    for key in WAF_STRING:
-        if key in resp.text:
-            ret["matched_string"] = key
-            ret["waf_detected"] = True
-            return ret
-    ret["waf_detected"] = False
-    return ret
+    ret["sock_ip"] = sock_ip
+    if sock_ip is not None and ipv6 is True and sock_ip.startswith("::ffff:"): # https://www.taobao.com/root.exe
+        ret["ns_record"] = ",".join(getipv6(tldextract.extract(url).fqdn))
+    
+    return waf_func(resp, ret)
 
+def writeExcel(future_results):
 
-proc = subprocess.run(["curl", "-6", "-s", "ifconfig.io"], stdout=subprocess.PIPE)
-if ":" not in proc.stdout.decode():
-    print("you network not support ipv6, you can get ipv6 through mobile hotspot.")
-    exit()
+    # create excel
+    file_name = time.strftime("%Y-%m-%d-%H-%M-%S.xlsx", time.localtime())
+    workbook = xlsxwriter.Workbook(file_name)
+    worksheet = workbook.add_worksheet()
 
-with open(FILE) as f:
-    url_list = f.readlines()
+    # excel title
+    bold = workbook.add_format({'bold': True, "align": "center"})
+    title = ['origin_url', 'is_ipv6', 'sock_ip', 'waf_detected', 'matched_string', 'status_code', 
+             'requests_error', 'ns_record', 'final_url']
+    for j, t in enumerate(title):
+        worksheet.write(0, j, t, bold)
 
-# create excel
-file_name = time.strftime("%Y-%m-%d-%H-%M-%S.xlsx", time.localtime())
-workbook = xlsxwriter.Workbook(file_name)
-worksheet = workbook.add_worksheet()
-row = 1
-col = 0
-
-# excel title
-bold = workbook.add_format({'bold': True, "align": "center"})
-title = ['origin_url', 'is_ipv6', 'dst_ip', 'waf_detected', 'matched_string', 'status_code', 'final_url',
-         'requests_error']
-for j, t in enumerate(title):
-    worksheet.write(0, j, t, bold)
-
-# TEST_URL_LIST = [
-#     "https://www.baidu.com",
-#     "https://www.qq.com",
-#     "https://www.taobao.com"
-# ]
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS) as executor:
-    # future_to_url = {executor.submit(send_req, url.strip(), IPV6): url  for url in TEST_URL_LIST}
-    future_to_url = {}
-
-    for url in url_list:
-        url = url.strip()
-        if not url.startswith("http"):
-            url += "http://"
-        aaaa_record = getipv6(tldextract.extract(url).fqdn)
-        if len(aaaa_record) != 0:
-            future_to_url[executor.submit(send_req, url, True)] = aaaa_record
-            future_to_url[executor.submit(send_req, url, False)] = aaaa_record
-        else:
-            future_to_url[executor.submit(send_req, url, False)] = aaaa_record
-
-    for future in concurrent.futures.as_completed(future_to_url):
-        aaaa_record = future_to_url[future]
+    # write to excel
+    row = 1
+    col = 0
+    for future in future_results:
         try:
             data = future.result()
         except Exception as exc:
-            print('%r generated an exception: %s' % (url, exc))
+            print('Exception: %s' % exc)
         else:
-            # data["AAAA_record"] = aaaa_record
             print(data)
-            # write to excel
             for line in data:
-                if line == "dst_ip" and data["is_ipv6"] is True and ":" not in data[line]:
-                    data[line] = ", ".join(aaaa_record)
                 if data[line] is not None:
                     worksheet.write(row, col, data[line])
                 col += 1
             col = 0
             row += 1
-workbook.close()
-print("Saved results to " + file_name)
 
-# with concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS) as executor:
-#     for result in executor.submit(send_req,URL_LIST):
-#         print(result)
+    workbook.close()
+    print("Saved results to " + file_name)
 
 
-# resp = req("https://www.taobao.com/root.exe",stream=True,allow_redirects=True)
-# print(resp.raw._connection.sock.getpeername())
+if __name__ == "__main__":
+    # print(send_req("http://ip.sb",True))
+    # print(send_req("http://ip.sb",False))
+
+    with open(FILE) as f:
+        url_list = f.readlines()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS) as executor:
+
+        # ipv4 checking...
+        future_to_url = {}
+
+        for url in url_list:
+            url = url.strip()
+            future_to_url[executor.submit(send_req, url, False)] = url
+        future_results = concurrent.futures.as_completed(future_to_url)
+
+        # ipv6 checking...
+        proc = subprocess.run(["curl", "-6", "-s", "ifconfig.io"], stdout=subprocess.PIPE)
+        if ":" not in proc.stdout.decode():
+            print("Your network not support ipv6, it will detect ipv4 only. you can get ipv6 through mobile hotspot.\n")
+            writeExcel(future_results)
+            exit()
+        for url in url_list:
+            url = url.strip()
+            aaaa_record = getipv6(tldextract.extract(url).fqdn)
+            if len(aaaa_record) != 0:
+                future_to_url[executor.submit(send_req, url, True)] = url
+        future_results = concurrent.futures.as_completed(future_to_url)
+        writeExcel(future_results)
