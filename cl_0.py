3e257ef0c6afb8955a505dd126a2e4708751aecf
cyal1 <example.qq.com>
2022-08-21 16:03:42 +0800

    update

--- cl_0.py
+++ cl_0.py
@@ -5,81 +5,84 @@ import urllib3
 from urllib3 import HTTPConnectionPool, HTTPSConnectionPool
 from urllib3.util import parse_url
 
+urllib3.disable_warnings()
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 " \
      "Safari/537.36 "
 
 
-def detect_cl_0(url):
+def detect_cl_0(url, path=None):
     u = parse_url(url)
-    target = ""
-    if u.scheme is not None:
-        target += u.scheme + u"://"
-    if u.auth is not None:
-        target += u.auth + u"@"
-    if u.host is not None:
-        target += u.host
-    if u.port is not None:
-        target += u":" + str(u.port)
-    target += path
-    http = urllib3.PoolManager()
-    try:
-        r = http.urlopen(method="GET",
-                         url=target,
-                         redirect=False,
-                         retries=0,
-                         timeout=5,
-                         body="body",
-                         headers={
-                             "Content-Length": "20",
-                             "User-Agent": UA,
-                         })
-    except Exception as e:
-        print(target, e.reason)
+    if path is None:
+        path = u.path
+    if u.scheme == 'https':
+        conn = HTTPSConnectionPool(u.host, u.port if u.port is not None else 443, cert_reqs='CERT_NONE',
+                                   assert_hostname=False)
     else:
-        scheme, host, port = r._pool.scheme, r._pool.host, r._pool.port
-        if scheme == 'https':
-            conn = HTTPSConnectionPool(host, port)
-        else:
-            conn = HTTPConnectionPool(host, port)
-        attacker = conn.urlopen(method="GET",
+        conn = HTTPConnectionPool(u.host, u.port if u.port is not None else 80)
+    try:
+        base_req = conn.urlopen(method=victim_method,
                                 url=path,
                                 redirect=False,
                                 retries=0,
-                                timeout=5,
-                                body="body",
+                                timeout=timeout,
                                 headers={
-                                    "Connection": "keep-alive",
-                                    "User-Agent": UA,
+                                    "User-Agent": UA
                                 })
-        victim = conn.urlopen(method="GET",
-                              url="/",
+    except Exception as e:
+        print(e)
+        # pass
+    else:
+        base_status, base_len = base_req.status, len(base_req.data)
+        attacker = conn.urlopen(method=attacker_method,
+                                url=path,
+                                redirect=False,
+                                retries=0,
+                                timeout=timeout,
+                                body="a b %",
+                                headers={
+                                    # "Content-Length": "10",
+                                    "Connection": "keep-alive",
+                                    "User-Agent": UA
+                                })
+        victim = conn.urlopen(method=victim_method,
+                              url=path,
                               redirect=False,
                               retries=0,
-                              timeout=5,
+                              timeout=timeout,
                               headers={
-                                  "User-Agent": UA,
+                                  "User-Agent": UA
                               })
 
-        if attacker.status != victim.status and victim.status != 200:
-            print("=" * 10)
-            print(target, "cl.0 detected", attacker.status, victim.status)
-            print("=" * 10)
+        if base_status != victim.status:
+            print(
+                f"\033[91m{url}, path:{path}, attacker:{attacker.status, len(victim.data)}, baseline:{base_status, base_len}, "
+                f"victim:{victim.status, len(victim.data)}, CL.0 detected!\033[0m")
+            print("HTTP/" + str(victim.version / 10), victim.status, victim.reason)
+            print('\r\n'.join([k + ': ' + victim.headers[k] for k in victim.headers]))
+            print(f"\r\n{victim.data.decode()}")
+
+        elif base_len != len(victim.data):
+            print(
+                f"\033[93m{url}, path:{path}, attacker:{attacker.status, len(victim.data)}, baseline:{base_status, base_len}, "
+                f"victim:{victim.status, len(victim.data)}, CL.0 detected!\033[0m")
         else:
-            print(target, "no cl_0")
+            print(
+                f"{url}, path:{path}, attacker:{attacker.status, len(victim.data)}, baseline:{base_status, base_len}, "
+                f"victim:{victim.status, len(victim.data)}, CL.0 Not Found")
+    finally:
+        conn.close()
 
 
-path = '/favicon.ico'
+victim_method = "GET"
+attacker_method = "POST"
+timeout = 35
+endpoint = '/favicon.ico'  # set endpoint None to use the url path.  /static/css/*.css , redirect / /en , /../
+
 if len(sys.argv) > 1:
-    path = sys.argv[1]
-
+    if sys.argv[1].startswith('http'):
+        detect_cl_0(sys.argv[1])
+        exit(0)
+    else:
+        endpoint = sys.argv[1]
 with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
-    # Start the load operations and mark each future with its URL
-    future_to_url = {executor.submit(detect_cl_0, url): url.strip() for url in sys.stdin}
-    # for future in concurrent.futures.as_completed(future_to_url):
-    #     url = future_to_url[future]
-    #     try:
-    #         data = future.result()
-    #     except Exception as exc:
-    #         print('%r generated an exception: %s' % (url, exc))
-    #     else:
-    #         print('%r page is %d bytes' % (url, len(data)))
+    future_to_url = {executor.submit(detect_cl_0, url.strip(), endpoint): url.strip() for url in sys.stdin}
